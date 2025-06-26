d0ab5061f96612e7c96a68fbd8f3c5e43a3c2dbb
bvnderb <brent.vd.bruggen@gmail.com>
2025-06-26 14:25:37 +0200

    updated the form to include an url to the job listing

--- job_applications/index.php
+++ job_applications/index.php
@@ -2,16 +2,17 @@
 // connect to the database
 require 'db.php'; 
 
-// checks if there was a request by submitting a form
-if ($_SERVER['REQUEST METHOD'] === 'POST')  {
+// checks if there was a request by a user submitting a form
+if ($_SERVER['REQUEST_METHOD'] === 'POST')  {
     $company = $_POST['compName'] ?? '';
     $location = $_POST['compLocation'] ?? '';
     $date = $_POST['applyDate'] ?? '';
+    $url = $_POST['vacancyUrl'] ?? '';
     $desc = $_POST['jobDesc'] ?? '';
 
 // security to prevent SQL injections
-$stmt = $pdo->prepare("INSERT INTO applications (company_name, company_location, description, date_applied) VALUES (?, ?, ?, ?)");
-$stmt->execute([$company, $location, $desc, $date]);
+$stmt = $pdo->prepare("INSERT INTO applications (company_name, company_location, description, date_applied, vacancy_url) VALUES (?, ?, ?, ?, ?)");
+$stmt->execute([$company, $location, $desc, $date, $url]);
 
 // user confirmation that the insert worked by showing a success message
 echo "<p>Application added for <strong>$company</strong>!</p>";
@@ -24,6 +25,7 @@ echo "<p>Application added for <strong>$company</strong>!</p>";
 <input type="text" name="compName" placeholder="Company name" required></input> </br>
 <input type="text" name="compLocation" placeholder="Location"></input> </br>
 <input type="date" name="applyDate" placeholder="Application date"></input> </br>
+<input type="url" name="vacancyUrl" placeholder="Provide an URL to the job listing"></input> </br>
 <textarea name="jobDesc" rows="5" cols="40" placeholder="Job description"></textarea> </br>
 <input type="submit" value="Add application"></input>
 </form> 
